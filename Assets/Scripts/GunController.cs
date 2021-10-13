3debaa49d53ac8b678be64ffe3949a8b064da298
mint-young <mintyoungs2@gmail.com>
2021-10-13 20:14:02 +0900

    Unity Tutorial2 Part5-2 Uncomplete

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -9,6 +9,13 @@ public class GunController : MonoBehaviour
     
    private float currentFireRate;
 
+   private bool isReload = false;
+
+   private bool isFineSightMode = false;
+    //본래 포지션 값.
+   [SerializeField]
+   private Vector3 originPos;
+
    private AudioSource audioSource;
 
    void Start(){
@@ -20,6 +27,8 @@ public class GunController : MonoBehaviour
     {
         GunFireRateCalc();  
         TryFire();
+        TryReload();
+        TryFineSight();
     }
 
     private void GunFireRateCalc(){
@@ -29,21 +38,64 @@ public class GunController : MonoBehaviour
     }
 
     private void TryFire(){
-        if(Input.GetButton("Fire1") && currentFireRate <= 0){
+        if(Input.GetButton("Fire1") && currentFireRate <= 0 && !isReload){
              Fire();
         }
     }
 
     private void Fire(){
-        currentFireRate = currentGun.fireRate;  
-        Shoot();
+
+        if(!isReload){
+            if(currentGun.currentBulletCount > 0)
+                Shoot();
+            else
+                StartCoroutine(ReloadCoroutine());
+        }
+        
     }
 
     private void Shoot(){
+        currentGun.currentBulletCount--;
+        currentFireRate = currentGun.fireRate; //연사 속도 재계산 
         PlaySE(currentGun.fire_Sound);
         currentGun.muzzleFlash.Play();
         Debug.Log("총알 발사함");
     }
+
+
+    private void TryReload(){
+        if(Input.GetKeyDown(KeyCode.R) && !isReload && currentGun.currentBulletCount < currentGun.reloadBulletCount){
+            StartCoroutine(ReloadCoroutine());
+        }
+    }
+
+    IEnumerator ReloadCoroutine(){
+        if(currentGun.carryBulletCount > 0){
+            isReload = true;
+            currentGun.anim.SetTrigger("Reload");
+
+            currentGun.carryBulletCount =   currentGun.currentBulletCount;
+            currentGun.currentBulletCount = 0;
+
+            yield return new WaitForSeconds(currentGun.reloadTime);
+
+            if(currentGun.carryBulletCount >= currentGun.reloadBulletCount){
+                currentGun.currentBulletCount = currentGun.reloadBulletCount;
+                currentGun.carryBulletCount -= currentGun.reloadBulletCount;
+            }
+            else{
+                currentGun.currentBulletCount = currentGun.carryBulletCount;
+                currentGun.carryBulletCount = 0;
+            }
+
+            isReload = false;
+        }
+        else{
+            Debug.Log("소유한 총알이 없습니다.");
+        }
+    }
+
+    
 
     private void PlaySE(AudioClip _clip){
         audioSource.clip = _clip;

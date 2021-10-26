504b00556832c69d3c6eb250434d3ca909a135d4
mint-young <mintyoungs2@gmail.com>
2021-10-26 15:31:38 +0900

    Unity Tutorial2 Part13-2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -193,7 +193,7 @@ public class PlayerController : MonoBehaviour
  
     // 점프 시도
     private void TryJump(){
-        if(Input.GetKeyDown(KeyCode.Space) && isGround){
+        if(Input.GetKeyDown(KeyCode.Space) && isGround && theStatusController.GetCurrentSP() > 0){
             Jump();
         }
     }
@@ -210,11 +210,11 @@ public class PlayerController : MonoBehaviour
 
     // 달리기 시도
     private void TryRun(){
-        if(Input.GetKey(KeyCode.LeftShift)){
+        if(Input.GetKey(KeyCode.LeftShift) && theStatusController.GetCurrentSP() > 0){
              Running(); 
         }
 
-        if(Input.GetKeyUp(KeyCode.LeftShift)){
+        if(Input.GetKeyUp(KeyCode.LeftShift) || theStatusController.GetCurrentSP() <= 0){
             RunningCancle();
         }
     }
--- Assets/Scripts/UI Script/StatusController.cs
+++ Assets/Scripts/UI Script/StatusController.cs
@@ -134,6 +134,69 @@ public class StatusController : MonoBehaviour
         images_Gauge[SATISFY].fillAmount = (float)currentSatisfy / satisfy;
     }
 
+    public void IncreaseHp(int _count){
+        if(currentHp + _count < hp){
+            currentHp += _count;
+        }else{
+            currentHp  = hp;
+        }
+    }
+
+    public void DecreaseHp(int _count){
+        if(currentDp > 0){
+            DecreaseDp(_count);
+            return;
+        }
+        currentHp -= _count;
+        if(currentHp <= 0)
+            Debug.Log("캐릭터의 hp가 0이 되었습니다!!");
+    }
+
+    public void IncreaseDp(int _count){
+        if(currentDp + _count < dp){
+            currentDp += _count;
+        }else{
+            currentDp  = dp;
+        }
+    }
+
+    public void DecreaseDp(int _count){
+        currentDp -= _count;
+        if(currentDp <= 0)
+            Debug.Log("캐릭터의 dp가 0이 되었습니다!!");
+    }
+
+    public void IncreaseHungry(int _count){
+        if(currentHungry + _count < hungry){
+            currentHungry += _count;
+        }else{
+            currentHungry  = hungry;
+        }
+    }
+
+    public void DecreaseHungry(int _count){
+        if(currentHungry - _count < 0){
+            currentHungry = 0;
+        }else
+            currentHungry -= _count;
+    }
+
+    public void IncreaseThirsty(int _count){
+        if(currentThirsty + _count < thirsty){
+            currentThirsty += _count;
+        }else{
+            currentThirsty  = thirsty;
+        }
+    }
+
+    public void DecreaseThirsty(int _count){
+            if(currentThirsty - _count < 0){
+                currentThirsty = 0;
+            }else
+                currentHungry -= _count;
+    }
+
+
     public void DecreaseStamina(int _count){
         spUsed = true;
         currentSpRechargeTime = 0;
@@ -145,5 +208,9 @@ public class StatusController : MonoBehaviour
         }
 
     }    
+
+    public int GetCurrentSP(){
+        return currentSp;
+    }
 
 }
